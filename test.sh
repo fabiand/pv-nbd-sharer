2b43e7e9a28bfe11519e9a6127569d8e6d59c67f
Fabian Deutsch <fabiand@fedoraproject.org>
2018-10-19 22:26:31 +0200

    ci: Fix test
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
-date > TEST.in
+head -c 512 /dev/urandom | tee TEST.in
 
 SOCKPATH=$PWD bash -x launcher serve_url TEST TEST.in &
 
 sleep 3
 
-qemu-img dd bs=1 if=nbd:unix:$PWD/TEST of=TEST.out
+qemu-img convert -O raw nbd:unix:$PWD/TEST TEST.out
 
 kill %1
 
-diff -au TEST.in TEST.out
+cmp TEST.in TEST.out
