6d86c1f2d2ceea1a009c6661e703b116ce636443
Fabian Deutsch <fabiand@fedoraproject.org>
2018-10-22 13:50:30 +0200

    ci: Tighten test
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- test.sh
+++ test.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 
+set -e
+
 # Multiple of 512B is important, as this is the read size of qemu-img convert/dd
 head -c 4096 /dev/urandom > TEST.in
 
 SOCKPATH=$PWD bash -x launcher serve_url TEST TEST.in &
 
-sleep 3
+sleep 2
 
 qemu-img convert -O raw nbd:unix:$PWD/TEST TEST.out
 
